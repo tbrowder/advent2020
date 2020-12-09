e5fa70f9e114b351c1ccb9e282fac90144fd7a51
Tom Browder <tom.browder@gmail.com>
2020-12-09 09:36:15 -0600

    add words and emoji place holders

--- view-advent.sh
+++ view-advent.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DIR=/home/tbrowde/mydata/tbrowde-home/raku-perl6/raku-advent/raku-advent-2020/advent2020
-FIL=$DIR/advent-pretty.html
+FIL=$DIR/fancy-advent.html
 
 # execute
 /bin/firefox file://$FIL
